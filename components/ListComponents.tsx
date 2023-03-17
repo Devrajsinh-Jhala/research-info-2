5e045247d9113571abe4e8fcbaf508403a82bef6
Devrajsinh Jhala <jhaladevrajsinh11@gmail.com>
2023-03-17 15:26:58 +0530

    Commits

--- components/ListComponents.tsx
+++ components/ListComponents.tsx
@@ -24,8 +24,7 @@ const ListComponents = ({ scholarships }: Props) => {
       </form>
 
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
-        {scholarships
-          .filter((post) => {
+        {scholarships?.filter((post) => {
             return search.toLowerCase() == ""
               ? post
               : post.title.toLowerCase().includes(search.toLowerCase());
